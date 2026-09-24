f5c2be44f481d3b399ff2effd1dc90b0e38ecc09
Lcat <62102830+Lcat12345@users.noreply.github.com>
2026-09-25 05:45:45 +0900

    Phase1_2.Output an Image (이미지출력)

--- Phase1/kernel.cu
+++ Phase1/kernel.cu
@@ -14,7 +14,33 @@ __global__ void addKernel(int *c, const int *a, const int *b)
 
 int main()
 {
-    std::cout << "Text Test~\n";
+    // Image;
+    int ImageWidth = 256;
+    int ImageHeight = 256;
+
+    // Render
+    std::cout << "P3\n" << ImageWidth << ' ' << ImageHeight << "\n255\n";
+
+    for (int j = 0; j < ImageHeight; j++)
+    {
+        std::clog << "\rScanlines remaining: " << (ImageHeight - j) << ' ' << std::flush;
+        for (int i = 0; i < ImageWidth; i++)
+        {
+            // r,g,b 값은 관례적으로 0과 1사이의 실수값으로 표현합니다.
+            auto r = double(i) / (ImageWidth - 1);
+            auto g = double(j) / (ImageHeight - 1);
+            auto b = 0.0;
+
+            // 출력을 할 때 0~255 사이의 정수값으로 스케일링 합니다.
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog << "\rDone.           \n";
 
     return 0;
 }
